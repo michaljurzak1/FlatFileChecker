a6b99bac5f2a51ac07e5b2b434fb4e2b2a424932
PRAKTYKA <PRAKTYKA@DELL3050>
2024-07-16 09:31:56 +0200

    Add project files.

---
+++ PlikPlaskiDownload/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PlikPlaskiDownload
+{
+    internal class Pobieranie
+    {
+        static void Main(string[] args)
+        {
+            Pobieranie ppd = new Pobieranie();
+            DateTime polish_local_time = DateTime.Now;
+            Console.WriteLine(ppd.Get_Flat_File_Url(polish_local_time);
+        }
+
+        private string Get_Flat_File_Url(DateTime date)
+        {
+            
+            
+            return "";
+        }
+
+    }
+}
