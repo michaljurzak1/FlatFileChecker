d222b0b5063f598fa1389122b475ee3928869130
michaljurzak1 <m.jurzak1@gmail.com>
2024-07-26 14:02:47 +0200

    Fixed dotnet test command not working by changing TestInitialize / Cleanup to ClassInitialize / Cleanup

--- FlatFileCheck.Tests/CheckDataSourceFactoryTests.cs
+++ FlatFileCheck.Tests/CheckDataSourceFactoryTests.cs
@@ -2,14 +2,15 @@
 using Microsoft.Data.Sqlite;
 using FlatFileDownload;
 using System.Data.Common;
+using System.Xml.Linq;
 
 namespace FlatFileCheck.Tests
 {
     [TestClass]
-    [assembly: CollectionBehavior(DisableTestParallelization = true)]
+    //[assembly: CollectionBehavior(DisableTestParallelization = true)]
     public class CheckDataSourceFactoryTests
     {
-        string[] skrotyPodatnikowCzynnych = new string[]
+        static string[] skrotyPodatnikowCzynnych = new string[]
             {
                 "140bebae8e72c6d3bbe805244870e3cb72f8408d6c6ea0eaa012347aaa686f3c94e63ff4e3fb3dd1297aa0337a03375deef687fbffca36ca5fffb1ed0d142774",
                 "0000037ffb0aac44690725f9968c1740c2d5c8780cfbd3ab85d60565fdf64e5da2066f18caf470463c66d18abd52b9c9c9b526501a0b9db319cd02d2b32c1151",
@@ -17,7 +18,7 @@ namespace FlatFileCheck.Tests
                 "0000139041e8726290e517ea01a8b011c84ed19c89ab764258b4fc35cfc81dc50188f546aece9c00cff881c7ab23a6078aa2ce645fdb6255921d4cd64a6bcabd",
             };
 
-        string[] skrotyPodatnikowZwolnionych = new string[]
+        static string[] skrotyPodatnikowZwolnionych = new string[]
         {
             "000034b37dd7ff72a1509f7f3fe1df4ce20105e40ddd663b00249158d993b938a1cb721bf6d60f91f5c43d0c0c3708491878d0f4c2e0060bb09613fc0c5cf981",
             "000055322b1ff5d4202b2bf1fd27ae27edd0e0ab5b66a53252dfe6d260184347e6d7d7b9b681ed3ea6ae3a946ce8a468e7072d85a5c1a74c257baafaf6cfdf73",
@@ -25,7 +26,7 @@ namespace FlatFileCheck.Tests
             "000094a9b9d5769b1372acc450d700b2cf60363ea1d7a872183c6b6f74d5b876d1a1df52775b6bd25619cde14cb392471f1be5802e4712e1fd63363215b9f743"
         };
 
-        string[] maski = new string[]
+        static string[] maski = new string[]
         {
                 "XX10100055YYYXXXXXXXXXXXXX",
                 "XX10100068YYYYXXXXXXXXXXXX",
@@ -37,30 +38,29 @@ namespace FlatFileCheck.Tests
         private static string DbName = "flatfileMock.db";
         private static string MockDbName = Path.Combine(new string[] { DbPath, DbName });
         
-        IConnection _connection;
-        private const string generationDate = "20240725";
-        private string nTransformations = "5000";
+        static IConnection _connection = new SqliteDB(false, DbPath, DbName, true);
+        static DownloadDataSourceFactory downloadFactory = new DownloadDataSourceFactory(_connection);
+        
 
-        [TestInitialize]
-        public void Init()
-        {
-            var tableDataPairs = new Dictionary<string, string[]>
+        private const string generationDate = "20240725";
+        private const string nTransformations = "5000";
+
+        static Dictionary<string, string[]> tableDataPairs = new Dictionary<string, string[]>
                 {
                     { "SkrotyPodatnikowCzynnych", skrotyPodatnikowCzynnych },
                     { "SkrotyPodatnikowZwolnionych", skrotyPodatnikowZwolnionych },
                     { "Maski", maski }
                 };
 
-            _connection = new SqliteDB(false, DbPath, DbName, true);
-
-            DownloadDataSourceFactory downloadFactory = new DownloadDataSourceFactory(_connection);
-
+        [ClassInitialize]
+        public static void Init(TestContext context)
+        {
             _connection.BulkInsert(tableDataPairs);
             downloadFactory.DaneInsertLoc(generationDate, nTransformations);
         }
 
-        [TestCleanup]
-        public void Cleanup()
+        [ClassCleanup]
+        public static void Cleanup()
         {
             // comment this line to keep the database after the test
             // however, it is not guaranteed that all test will have access
@@ -70,7 +70,7 @@ namespace FlatFileCheck.Tests
         }
 
         [TestMethod]
-        [assembly: DoNotParallelize]
+        //[assembly: DoNotParallelize]
         public void IsDataValidTest()
         {
 
@@ -83,7 +83,7 @@ namespace FlatFileCheck.Tests
         }
 
         [TestMethod]
-        [assembly: DoNotParallelize]
+        //[assembly: DoNotParallelize]
         public void CountDataTest()
         {
             var factory = new CheckDataSourceFactory(_connection);
@@ -95,7 +95,7 @@ namespace FlatFileCheck.Tests
         }
 
         [TestMethod]
-        [assembly: DoNotParallelize]
+        //[assembly: DoNotParallelize]
         [DataRow("5512393471", "51 1950 0001 2006 0073 4183 0002", "\nReal Account in SkrotyPodatnikowCzynnych")]
         [DataRow("7740001454", "71124069606666000007118600", "\nVirtual Account in SkrotyPodatnikowZwolnionych")]
         public void CheckAccountTest(string nip, string nrb, string response)
@@ -109,7 +109,7 @@ namespace FlatFileCheck.Tests
         }
 
         [TestMethod]
-        [assembly: DoNotParallelize]
+        //[assembly: DoNotParallelize]
         [DataRow("SkrotyPodatnikowCzynnych", "0000123602993c214cf508b75276a5d8c18aa96d9e806fb35dce9084768c73aaf423eda9517854161514ee04918c8e8b5aba5b5ded408ec86089f93cf65daa8c", "value")]
         [DataRow("SkrotyPodatnikowZwolnionych", "000034b37dd7ff72a1509f7f3fe1df4ce20105e40ddd663b00249158d993b938a1cb721bf6d60f91f5c43d0c0c3708491878d0f4c2e0060bb09613fc0c5cf981", "value")]
         [DataRow("Maski", "XX12406960YYYXXXXXXXXXXXXX", "value")]
@@ -122,20 +122,5 @@ namespace FlatFileCheck.Tests
 
             Assert.IsTrue(result, "CheckRecordInTable not working properly.");
         }
-
-        /*
-        [TestMethod]
-        [DataRow("5252344078", "37103015080000000504162044")]
-        public async Task CheckApi(string nip, string nrb)
-        {
-            bool? result = await MinisterstwoFinansowApi.GetAccount(nip, nrb);
-            if (result == null)
-            {
-                Console.WriteLine("Error regarding Api connection.");
-                Assert.Fail();
-            }
-            Assert.IsTrue(result.Value, "Api check failed.");
-            Console.WriteLine(result);
-        }*/
     }
 }
